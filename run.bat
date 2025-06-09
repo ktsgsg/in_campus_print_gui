5b975b5b72452d658e3397097e99b27a82de2c4e
Keita <184593592+ktsgsg@users.noreply.github.com>
2025-06-09 10:52:27 +0900

    修正

--- run.bat
+++ run.bat
@@ -3,5 +3,6 @@ pip install textual
 pip install requests
 pip install selenium
 pip install requests-toolbelt
+pip install cryptography
 python main.py
 pause
--- run.command
+++ run.command
@@ -4,4 +4,5 @@ pip install textual
 pip install requests
 pip install selenium
 pip install requests-toolbelt
+pip install cryptography
 python3 main.py
